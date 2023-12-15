861adb776da76cf1f34cbce45836afa65754acd5
meidanlong <meidanlong@gmail.com>
2023-12-15 12:33:58 +0800

    更新环境配置

--- sync/sync.sh
+++ sync/sync.sh
@@ -4,9 +4,9 @@
 export yuque_access_token=8RJsszjXfqrpxbsCW8RqmgtEQsfmeiNuyPZAKME1
 
 # 正式workflows
-#export PYTHONPATH=./
-#python sync/get_yuque_docs.py
+export PYTHONPATH=./
+python sync/get_yuque_docs.py
 
 # 本地环境
-export PYTHONPATH=../
-python get_yuque_docs.py
+#export PYTHONPATH=../
+#python get_yuque_docs.py
