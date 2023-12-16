ef66d1e80ba18fed57481e40a41b0b4ebe422946
meidanlong <meidanlong@gmail.com>
2023-12-16 22:20:53 +0800

    sync-job

--- sync/sync.sh
+++ sync/sync.sh
@@ -16,6 +16,8 @@ python sync/get_yuque_docs.py
 
 # 同步github
 git remote set-url origin 'https://'${github_token}'@'${github_git}
+git config --global user.email "meidanlong@gmail.com"
+git config --global user.name "meidanlong"
 git add .
 git commit -m "sync-job"
 git remote -v
