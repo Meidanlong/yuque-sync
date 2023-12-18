2d3c079daec6c3fd9329bb9d8a334f2a9faffe84
meidanlong <meidanlong@maoyan.com>
2023-12-18 21:51:29 +0800

    修复git提交目录问题

--- sync/service/github_service.py
+++ sync/service/github_service.py
@@ -12,21 +12,13 @@ def get_github_repo():
 
 def push_github_origin(file_path: str, file_content: str, commit_message: str):
     repo = get_github_repo()
+    total_file_path = 'content/{file_path}'.format(file_path=file_path)
     try:
         # 检查文件是否在仓库中
-        contents = repo.get_contents('content/{file_path}'.format(file_path=file_path), ref="refs/heads/" + REPO_BRANCH)
+        contents = repo.get_contents(total_file_path, ref="refs/heads/" + REPO_BRANCH)
         repo.update_file(contents.path, commit_message, file_content, contents.sha, branch=REPO_BRANCH)
         print(f"文件 '{file_path}' 已更新.")
     except UnknownObjectException as e:
         # 如果文件不存在，那就创建它
-        repo.create_file(file_path, commit_message, file_content, branch=REPO_BRANCH)
+        repo.create_file(total_file_path, commit_message, file_content, branch=REPO_BRANCH)
         print(f"文件 '{file_path}' 已创建.")
-
-
-if __name__ == '__main__':
-    # 文件路径和文件内容
-    file_path = "content/posts/dew.md"
-    file_content = "## File content 更新"
-    # 提交信息
-    commit_message = "deploy"
-    push_github_origin(file_path, file_content, commit_message)
