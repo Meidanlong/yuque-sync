528ee53f1ad19e0076931aea517b6b2117607377
meidanlong <meidanlong@maoyan.com>
2023-12-18 09:50:35 +0800

    update

--- sync/domain/constant/contants.py
+++
@@ -1,6 +0,0 @@
-
-# 日期格式化
-DATE_FORMAT = '%Y%m%dT%H:%M:%S'
-
-GET_REPO_LIST_URL = 'https://www.yuque.com/api/v2/users/{login}/repos'
-GET_BOOK_LIST_URL = 'https://www.yuque.com/api/v2/repos/{book_id}/docs'
