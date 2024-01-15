9c1e5eb6f3cff39c56a3d3ccaf5d0f3abc95c633
meidanlong <meidanlong@maoyan.com>
2024-01-15 10:33:56 +0800

    更新执行脚本

--- sync/sync_local.sh
+++ sync/sync_local.sh
@@ -2,8 +2,8 @@
 
 export PYTHONPATH=../
 
-python -c "from app import sync_yuque; sync_yuque()"
+python -c "from sync.app import yuque_sync; yuque_sync()"
 
 hugo deploy
 
-python -c "from app import push_pages; push_pages()"
+python -c "from sync.app import push_pages; push_pages()"
